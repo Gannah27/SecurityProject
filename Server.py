fb218b5c760a89a8d578e6c89503f5c25d5356d7
Gannah Mohamed Gaber <gannahmohamed2001@gmail.com>
2024-05-11 02:28:05 +0300

    added client server

---
+++ Server.py
@@ -0,0 +1,27 @@
+import socket
+
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+host = socket.gethostname()
+port = 12345
+
+
+server_socket.bind((host, port))
+
+
+server_socket.listen(5)
+
+print("Server listening on {}:{}".format(host, port))
+
+while True:
+
+    client_socket, addr = server_socket.accept()
+    print('Got connection from', addr)
+
+    #send and receive between two clients
+
+
+
+    client_socket.close()
--- authentication_module.py
+++ authentication_module.py
@@ -1,4 +1,7 @@
 import pyrebase
+import socket
+
+
 
 firebaseCon={
   'apiKey': "AIzaSyBygXEE7-GG97RJMNEYBGIuyDN71OBoweo",
@@ -19,9 +22,10 @@ def login():
     try:
         user = auth.sign_in_with_email_and_password(email, password)
         print("Successfully logged in")
+        return 0
     except:
         print("invalid email or password")
-    return
+        return 1
 
 def signup():
     print("sign up..")
@@ -30,13 +34,39 @@ def signup():
     try:
         user = auth.create_user_with_email_and_password(email,password)
         print("account made successfully")
+        return 0
     except:
         print("Email already exists")
-    return
+        return 1
 
 ans= input("Are you a new user?[y/n]")
+if __name__ == "__main__":
+        if ans =='n':
+            while True:
+                flag1 = login()
+                if (flag1):
+                    print("please renter your credentials:")
+                    login()
+                else:
+                    break
+        elif ans =='y':
+            while True:
+                flag1 = signup()
+                if(flag1):
+                    print("please enter other email:")
+                    signup()
+                else:
+                    break
 
-if ans =='n':
-    login()
-elif ans =='y':
-    signup()
+        # would be server between 2 clients
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        host = socket.gethostname()
+        port = 12345
+        client_socket.connect((host, port))
+
+        msg = client_socket.recv(1024)
+
+       #send to server data
+        #receive from server
+
+        client_socket.close()
